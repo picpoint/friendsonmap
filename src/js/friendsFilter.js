63865396ff339cbd0d93f0e06b52254f77a92fbe
rmtar <picpoint@yandex.ru>
2020-01-16 23:25:33 +0300

    work with new task - friends on map

---
+++ src/js/friendsFilter.js
@@ -0,0 +1,103 @@
+let keyAPI = '0735696f-0a98-472f-9ffc-143c71d3f506';
+
+new Promise((resolve) => {	                                                          	// создаём промис
+  window.addEventListener('load', () => {		                                            // по событию загрузки страницы разрешаем промис
+    resolve();
+  });  
+})
+  .then(() => {		                                                                      // тогда 
+    return new Promise((resolve, reject) => {		                                        // новый промис
+      VK.init({		                                                                      // инициализируем вк по определённому ID
+        apiId: 7250280
+      });
+
+      VK.Auth.login((response) => {		                                                  // авторизуемся
+        //console.log(response);
+        if(response.session) {                                                          // если прошла авторизация и сессия доступна
+          resolve(response);                                                            // резолвим ответ          
+        } else {
+          reject(new Error(`***ERR TO AUTHORIZ***`));                                   // иначе ошибка авторизации
+        }
+      }, 2);	                                                                          // 2 - это передаваймый параметр, указывающий что нужен доступ к списку друзей
+      
+    });
+  })
+  .then(() => {
+    return new Promise((resolve, reject) => {	                                          // тогда возвращаем новый промис
+      VK.api('users.get', {v: '5.8', name_case: 'gen'}, (response) => {	                // указываем версию апи и передача имени в родительном падеже
+        //console.log(response);
+        if(response.error) {                                                            // если промис не разрешился
+          reject(new Error(response.error.error_msg));                                  // реджектим
+        } else {          
+          resolve(response);                                                            // резолвим ответ
+        }
+      });
+    });
+  })
+  .then(() => {
+    return new Promise((resolve, reject) => {                                            // возвращаем новый промис
+      VK.api('friends.get', {v: '5.8', fields: 'photo_100'}, (response) => {             // передаём параметры для выгрузки фото 
+        if(response.error) {                                                             // если ошибка ответа
+          reject(new Error(response.error.error_msg));                                   // реджектим ответ  
+        } else {
+          resolve(response);                                                             // иначе респонсим ответ
+        }
+      });
+    });
+  })
+  .then((response) => {                                                                  // читаем ответ    
+    console.log(response);
+
+    ymaps.ready(init);                                                                   // инициализируем переменную ymaps
+
+    function init() {
+      var myMap = new ymaps.Map("map", 
+        {
+         center: [55.76, 37.64],
+         zoom: 10
+        }, 
+        {
+          searchControlProvider: 'yandex#search'
+        }
+      ),
+
+    // Создаем геообъект с типом геометрии "Точка".
+        myGeoObject = new ymaps.GeoObject(
+          {
+            // Описание геометрии.
+            geometry: {
+                type: "Point",
+                coordinates: [55.8, 37.8]
+            },
+            // Свойства.
+            properties: {
+                // Контент метки.
+                iconContent: 'Я тащусь',
+                hintContent: 'Ну давай уже тащи'
+            }
+          }, 
+          {
+            // Опции.
+            // Иконка метки будет растягиваться под размер ее содержимого.
+            preset: 'islands#blackStretchyIcon',
+            // Метку можно перемещать.
+            draggable: true
+          }
+        ),
+        
+        myPieChart = new ymaps.Placemark();
+
+    myMap.geoObjects
+        .add(myGeoObject)
+        .add(myPieChart)
+        .add(new ymaps.Placemark([55.684758, 37.738521], {
+            balloonContent: 'цвет <strong>воды пляжа бонди</strong>'
+        }, {
+            preset: 'islands#icon',
+            iconColor: '#0095b6'
+        }));
+        
+}
+
+    
+  });
